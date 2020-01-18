dd4ca1ff6edc4a3edbadb522844416d423f07939
iwanttobealight <iwanttobealight@yandex.ru>
2020-01-19 01:11:16 +0300

    inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## development
+```js
+  yarn dev
+```
+
+# Web Workers Demo
+Migrate Long Running JS onto a Web Worker
+
+### [Relevant Quiz from Browser Rendering Optimization](https://www.udacity.com/course/viewer#!/c-ud860/l-4138168623/e-4184098558/m-4150829139)
+
---
+++ scripts/worker.js
@@ -0,0 +1,34 @@
+// importScripts('imageManips.js');
+import { manipulate } from './imageManips'
+
+
+onmessage = function (e) {
+  const imageData = e.data.data;
+  const type = e.data.type;
+
+  try {
+    let length = imageData.data.length / 4;
+    let j, i, ref, r, g, b, a
+
+
+    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+      r = imageData.data[i * 4 + 0];
+      g = imageData.data[i * 4 + 1];
+      b = imageData.data[i * 4 + 2];
+      a = imageData.data[i * 4 + 3];
+      let pixel = manipulate(type, r, g, b, a);
+      imageData.data[i * 4 + 0] = pixel[0];
+      imageData.data[i * 4 + 1] = pixel[1];
+      imageData.data[i * 4 + 2] = pixel[2];
+      imageData.data[i * 4 + 3] = pixel[3];
+    }
+    postMessage(imageData);
+  } catch (e) {
+    function ManipulationException(message) {
+      this.name = "ManipulationException";
+      this.message = message;
+    };
+    throw new ManipulationException('Image manipulation error');
+    postMessage(undefined);
+  }
+}
